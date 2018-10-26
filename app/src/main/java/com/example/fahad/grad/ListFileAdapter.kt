27ad948a6c1755d0d24393d2fd825a6bde42c996
Abdulmajeed <aabdulmajeed16@gmail.com>
2018-10-26 14:59:07 +0300

    Add classes: File, Adapter Changes on Home, Login activities

---
+++ app/src/main/java/com/example/fahad/grad/ListFileAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.fahad.grad
+
+import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class UsersRecyclerAdapter(private val listFile: List<File>) : RecyclerView.Adapter<UsersRecyclerAdapter.FileViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
+        // inflating recycler item view
+        val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.file_row, parent, false)
+
+        return FileViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
+        holder.fileName.text = listFile[position].Name
+        holder.fileDescription.text = listFile[position].Description
+    }
+
+    override fun getItemCount(): Int {
+        return listFile.size
+    }
+
+
+    /**
+     * ViewHolder class
+     */
+    inner class FileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var fileName: AppCompatTextView
+        var fileDescription: AppCompatTextView
+
+        init {
+            fileName = view.findViewById<View>(R.id.fileName) as AppCompatTextView
+            fileDescription = view.findViewById<View>(R.id.fileDescription) as AppCompatTextView
+        }
+    }
+
+
+}
